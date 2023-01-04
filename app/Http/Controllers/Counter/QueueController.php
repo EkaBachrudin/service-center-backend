bf84de6d65de973f6e83634689edef86ea3c5e97
Eka Bachrudin Nursa <ekabachrudin@gmail.com>
2023-01-05 01:31:57 +0700

    Random

--- app/Http/Controllers/Counter/QueueController.php
+++ app/Http/Controllers/Counter/QueueController.php
@@ -187,6 +187,12 @@ class QueueController extends Controller
         ]);
     }
 
+
+
+
+
+
+
     private function getWittingStatus($id)
     {
         $query = Queue::query()->where('counters_id', $id)
